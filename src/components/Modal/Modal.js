44216d26707824bac2307c992101d66eb4fd507d
saxenanihal95 <saxenanihal95@gmail.com>
2020-12-29 08:45:32 +0530

    Add to Basket

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,33 +1,12 @@
 import React from "react";
 import "./Modal.scss";
-export default function Modal({ visible, children, onClose }) {
+export default function Modal({ visible, children, onClose, title = "" }) {
   if (!visible) {
     return null;
   }
   return (
     <div class="modal" id="modal">
-      <div class="content">
-        <span
-          class="close"
-          onClick={(e) => {
-            onClose && onClose(e);
-          }}
-        >
-          &times;
-        </span>
-        <h2>Modal Window</h2>
-        {children}
-        {/* <div class="actions">
-          <button
-            class="toggle-button"
-            onClick={(e) => {
-              onClose && onClose(e);
-            }}
-          >
-            close
-          </button>
-        </div> */}
-      </div>
+      <div class="content">{children}</div>
     </div>
   );
 }
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,6 +4,7 @@ import Grid from "../Grid/Grid";
 import SubTitle from "../SubTitle/SubTitle";
 import Title from "../Title/Title";
 import "./Section.scss";
+import { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft";
 
 import { createServer } from "miragejs";
 import Modal from "../Modal/Modal";
@@ -22,14 +23,14 @@ server.get("/api/sections", {
           title: "Any Burrito + Soft drink + Brownies",
           subTitle:
             "Each Burrito, Tortilla filled with rice, pinto beans, with choice of main filling, Pico de Gallo, Cheese and Guacamole as standard.",
-          price: "£11.00",
+          price: "11.00",
         },
         {
           id: 2,
           title: "2 Burritos + Soft Drink + Brownie",
           subTitle:
             "2 Burritos + 2 Soft Drink + 2 Brownie Each Burrito, Tortilla filled with rice, pinto beans, with choice of main filling, Pico de Gallo, Cheese and Guacamole as standard.",
-          price: "£20.00",
+          price: "20.00",
         },
       ],
     },
@@ -43,21 +44,21 @@ server.get("/api/sections", {
           title: "Carne Burrito",
           subTitle:
             "Large tortilla filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce, sour cream, cheese and guacamole. (Vegan burritos come with vegan cheese and sour cream).",
-          price: "£7.00",
+          price: "7.00",
         },
         {
           id: 2,
           title: "Vegan Burrito Bowl",
           subTitle:
             "No Tortilla, but more Filling. Bowl filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce (as above), Vegan Mayo, Vegan cheese and guacamole.",
-          price: "£7.80",
+          price: "7.80",
         },
         {
           id: 3,
           title: "Veggie Burrito Bowl",
           subTitle:
             "No Tortilla, but more Filling. Bowl filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce (as above), sour cream, cheese and guacamole.",
-          price: "£7.80",
+          price: "7.80",
         },
       ],
     },
@@ -72,21 +73,21 @@ server.get("/api/sections", {
           title: "Grande Burrito Bowl",
           subTitle:
             "No Tortilla, but more Filling. Bowl filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce (as above), sour cream, cheese and guacamole.",
-          price: "£7.80",
+          price: "7.80",
         },
         {
           id: 2,
           title: "Vegan Burrito Bowl",
           subTitle:
             "No Tortilla, but more Filling. Bowl filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce (as above), sour cream, cheese and guacamole.",
-          price: "£7.80",
+          price: "7.80",
         },
         {
           id: 3,
           title: "Veggie Burrito Bowl",
           subTitle:
             "No Tortilla, but more Filling. Bowl filled with choice of main filling, Mexican rice, pinto beans, onions and peppers, Pico de gallo, spicy sauce (as above), sour cream, cheese and guacamole.",
-          price: "£7.80",
+          price: "7.80",
         },
       ],
     },
@@ -110,6 +111,12 @@ server.get("/api/sections", {
 export default function Section() {
   const [sections, setSections] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [chooseFavourite, setChooseFavourite] = useState(false);
+  const [seletectedItem, setSelectedItem] = useState({});
+  const [activeTabIndex, setActiveTabIndex] = useState(1);
+  const [count, setCount] = useState({});
+  const [sectionId, setSectionId] = useState(null);
+  const [basket, setBasket] = useState({});
   useEffect(() => {
     fetch("/api/sections")
       .then((res) => res.json())
@@ -118,14 +125,160 @@ export default function Section() {
       });
   }, []);
 
-  console.log(sections);
+  const pizzas = [
+    { title: "Margerite" },
+    { title: "Margerite" },
+    { title: "Margerite" },
+    { title: "Margerite" },
+    { title: "Margerite" },
+  ];
+
+  const addons = [
+    { title: "French Fries", price: 4 },
+    { title: "Green Salad", price: 3 },
+    { title: "Tomato", price: 2 },
+  ];
+
+  const currentCount = count?.[sectionId]?.[seletectedItem?.id];
+
+  const basketContent = Object.keys(basket).flatMap((sectionId) =>
+    Object.keys(basket[sectionId]).map((itemId) => basket[sectionId][itemId])
+  );
+
+  console.log(basket);
 
   return (
     <>
-      <Modal
-        visible={modalVisible}
-        onClose={() => setModalVisible(false)}
-      ></Modal>
+      <Modal visible={modalVisible}>
+        {chooseFavourite ? (
+          <div>
+            <div className="ModalTitle">
+              <div className="BackIcon">
+                <FaArrowLeft onClick={() => setChooseFavourite(false)} />
+              </div>
+              <h2>Choose you favourite</h2>
+              <span class="close" onClick={() => setModalVisible(false)}>
+                &times;
+              </span>
+            </div>
+            <div className="Tabs">
+              <div
+                className={`Tab${activeTabIndex === 1 ? " active" : ""}`}
+                onClick={() => setActiveTabIndex(1)}
+              >
+                Pizza
+              </div>
+              <div
+                className={`Tab${activeTabIndex === 2 ? " active" : ""}`}
+                onClick={() => setActiveTabIndex(2)}
+              >
+                Extra
+              </div>
+            </div>
+            {activeTabIndex === 1 && (
+              <div className="PizzaContainer">
+                {pizzas.map((pizza) => (
+                  <div
+                    className="PizzaItem"
+                    onClick={() => setActiveTabIndex(2)}
+                  >
+                    {pizza.title}
+                  </div>
+                ))}
+              </div>
+            )}
+            {activeTabIndex === 2 && (
+              <div className="PizzaContainer">
+                {addons.map((addon) => (
+                  <div
+                    className="PizzaItem"
+                    onClick={() => setActiveTabIndex(2)}
+                  >
+                    {addon.title}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <div className="ModalTitle">
+              <h2>Add Item to Basket</h2>
+              <span class="close" onClick={() => setModalVisible(false)}>
+                &times;
+              </span>
+            </div>
+            <div className="ModalBody">
+              <div className="ItemHeader">
+                <p className="heading">{seletectedItem.title}</p>
+                <p>{seletectedItem.price}</p>
+              </div>
+              <div>
+                <p>Choose you favourites</p>
+                <p>Margereta</p>
+                <p onClick={() => setChooseFavourite(true)}>
+                  Choose your second pizza
+                </p>
+              </div>
+            </div>
+            <div className="ModalFooter">
+              <div className="OrderButtonContainer">
+                <div
+                  className={
+                    currentCount < 2 ? "Decreament disabled" : "Decreament"
+                  }
+                  onClick={() =>
+                    currentCount < 2
+                      ? null
+                      : setCount({
+                          [sectionId]: {
+                            [seletectedItem.id]: currentCount - 1,
+                          },
+                        })
+                  }
+                >
+                  -
+                </div>
+                <div>{currentCount}</div>
+                <div
+                  className="Icreament"
+                  onClick={() =>
+                    setCount({
+                      [sectionId]: {
+                        [seletectedItem.id]: currentCount + 1,
+                      },
+                    })
+                  }
+                >
+                  +
+                </div>
+              </div>
+              <div
+                className="AddToBasketButton"
+                onClick={() => {
+                  setBasket({
+                    ...basket,
+                    [sectionId]: {
+                      ...basket?.[sectionId],
+                      [seletectedItem.id]: {
+                        ...basket?.[sectionId]?.[seletectedItem.id],
+                        item: {
+                          title: seletectedItem.title,
+                          price: seletectedItem.price * currentCount,
+                        },
+                      },
+                    },
+                  });
+                  setModalVisible(false);
+                }}
+              >
+                <p className="text">Add To Basket </p>
+                {seletectedItem.price * currentCount}
+              </div>
+            </div>
+          </div>
+        )}
+      </Modal>
       <div className="Section">
         <div>
           {sections.map((section) => (
@@ -134,7 +287,14 @@ export default function Section() {
               <SubTitle text={section.subTitle} />
               <Grid>
                 {section.items.map((item) => (
-                  <Card onClick={() => setModalVisible(true)}>
+                  <Card
+                    onClick={() => {
+                      setSelectedItem(item);
+                      setSectionId(section.id);
+                      setCount({ [section.id]: { [item.id]: 1 } });
+                      setModalVisible(true);
+                    }}
+                  >
                     <p>{item.title}</p>
                     <p>{item.subTitle}</p>
                     <p>{item.price}</p>
@@ -147,6 +307,12 @@ export default function Section() {
         <div className="OrderContainer">
           <Card>
             <p>Your Basket</p>
+            {basketContent.map(({ item }) => (
+              <div>
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+              </div>
+            ))}
           </Card>
         </div>
       </div>
